6a5f6401ede3ad2fba799d994ddd6fafae565ca9
Ibrahim <ibrahim.bazzah@edu-kw1c.nl>
2020-03-19 11:53:23 +0100

    opdracht 2.2

--- Hoofdstuk 2/Huiswerk/2.2.php
+++ Hoofdstuk 2/Huiswerk/2.2.php
@@ -1,4 +1,19 @@
 <?php
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = "<br>";
+$text13 = "is";
+$text14 = "Vind je niet";
+$text15 = "toch";
 
-
+echo("<p>" . $text1 . " " . $text4 . " " . $text13 . " " . $text5 . " " . $text3 . $text12 . $text7 . $text10 . $text8 . " " . $text15 . $text9. $text12 . " " . $text6 . " " . $text5 . " " . $text3 . $text10 . "</p>");
 ?>
