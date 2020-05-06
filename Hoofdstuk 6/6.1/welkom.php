407e7489fc59cb113142a144a9a313a1398bf177
Ibrahim <ibrahim.bazzah@edu-kw1c.nl>
2020-05-06 16:15:44 +0200

    opdracht 2.2

---
+++ Hoofdstuk 6/6.1/welkom.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="../../Index/index.css">
+    <title>Opdrachten PHP periode 3</title>
+</head>
+<body>
+<h1>Welkom</h1>
+<?php
+
+echo "Welkom: " . $_SESSION["username"];
+
+?>
+
+</body>
+</html>
---
+++ Hoofdstuk 6/6.2/welkomcookie.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="../css/style.css">
+    <meta charset="utf-8" />
+    <title>Opdrachten PHP periode 3</title>
+</head>
+<body>
+<?php
+
+echo "Welkom: " . $_COOKIE["username"];
+?>
+</body>
+</html>
